d9a1c2bd9d7c8ad59efbc38132fb0486d26ffa6e
unknown <Laxmi6771>
2025-12-19 22:26:51 +0530

    Conditional rendering status toggle component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import Counter from "./Counter";
+import { useState } from "react";
+import ComponentA from "./ComponentA";
+import ComponentB from "./ComponentB";
 
 function App() {
+  const [status, setStatus] = useState(false);
+
   return (
-    <div>
-      <Counter />
+    <div style={{ padding: "20px" }}>
+      <button onClick={() => setStatus(!status)}>
+        Toggle Status
+      </button>
+
+      <br /><br />
+
+      {status ? <ComponentA /> : <ComponentB />}
     </div>
   );
 }
